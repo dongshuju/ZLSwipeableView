1d6eef24fa96880044dc34bb5bb415b8f555cc11
wangkang <wangkang@p1.com>
2018-03-01 17:01:48 +0800

    change swpie gesture

--- ZLSwipeableView/ViewManager.m
+++ ZLSwipeableView/ViewManager.m
@@ -26,6 +26,7 @@ static const CGFloat kAnchorViewWidth = 1000;
 @property (strong, nonatomic) UISnapBehavior *snapBehavior;
 @property (strong, nonatomic) UIAttachmentBehavior *viewToAnchorViewAttachmentBehavior;
 @property (strong, nonatomic) UIAttachmentBehavior *anchorViewToPointAttachmentBehavior;
+@property (strong, nonatomic) UIDynamicItemBehavior *anchorViewDynamicItemBehavior;
 @property (strong, nonatomic) UIPushBehavior *pushBehavior;
 
 @end
@@ -38,257 +39,269 @@ static const CGFloat kAnchorViewWidth = 1000;
            miscContainerView:(UIView *)miscContainerView
                     animator:(UIDynamicAnimator *)animator
                swipeableView:(ZLSwipeableView *)swipeableView {
-    self = [super init];
-    if (self) {
-        self.view = view;
-        self.containerView = containerView;
-        self.miscContainerView = miscContainerView;
-        self.animator = animator;
-        self.swipeableView = swipeableView;
-
-        [view addGestureRecognizer:
-                  [[PanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)]];
-        _anchorView =
-            [[UIView alloc] initWithFrame:CGRectMake(0, 0, kAnchorViewWidth, kAnchorViewWidth)];
-        _anchorView.hidden = NO;
-        [miscContainerView addSubview:_anchorView];
-        [containerView insertSubview:view atIndex:index];
-    }
-    return self;
+  self = [super init];
+  if (self) {
+    self.view = view;
+    self.containerView = containerView;
+    self.miscContainerView = miscContainerView;
+    self.animator = animator;
+    self.swipeableView = swipeableView;
+    
+    [view addGestureRecognizer:
+     [[PanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)]];
+    _anchorView =
+    [[UIView alloc] initWithFrame:CGRectMake(0, 0, kAnchorViewWidth, kAnchorViewWidth)];
+    _anchorView.hidden = NO;
+    [miscContainerView addSubview:_anchorView];
+    [containerView insertSubview:view atIndex:index];
+  }
+  return self;
 }
 
 - (void)dealloc {
-    for (UIGestureRecognizer *aGestureRecognizer in _view.gestureRecognizers) {
-        if ([aGestureRecognizer isKindOfClass:[PanGestureRecognizer class]]) {
-            [_view removeGestureRecognizer:aGestureRecognizer];
-        }
+  for (UIGestureRecognizer *aGestureRecognizer in _view.gestureRecognizers) {
+    if ([aGestureRecognizer isKindOfClass:[PanGestureRecognizer class]]) {
+      [_view removeGestureRecognizer:aGestureRecognizer];
     }
-
-    if (_snapBehavior) {
-        [_animator removeBehavior:_snapBehavior];
-    }
-    if (_viewToAnchorViewAttachmentBehavior) {
-        [_animator removeBehavior:_viewToAnchorViewAttachmentBehavior];
-    }
-    if (_anchorViewToPointAttachmentBehavior) {
-        [_animator removeBehavior:_anchorViewToPointAttachmentBehavior];
-    }
-    if (_pushBehavior) {
-        [_animator removeBehavior:_pushBehavior];
-    }
-
-    [_anchorView removeFromSuperview];
-    [_view removeFromSuperview];
+  }
+  
+  if (_snapBehavior) {
+    [_animator removeBehavior:_snapBehavior];
+  }
+  if (_viewToAnchorViewAttachmentBehavior) {
+    [_animator removeBehavior:_viewToAnchorViewAttachmentBehavior];
+  }
+  if (_anchorViewToPointAttachmentBehavior) {
+    [_animator removeBehavior:_anchorViewToPointAttachmentBehavior];
+  }
+  if (_anchorViewDynamicItemBehavior) {
+    [_animator removeBehavior:_anchorViewDynamicItemBehavior];
+  }
+  if (_pushBehavior) {
+    [_animator removeBehavior:_pushBehavior];
+  }
+  
+  [_anchorView removeFromSuperview];
+  [_view removeFromSuperview];
 }
 
 #pragma mark - Action
 
 - (void)handlePan:(UIPanGestureRecognizer *)recognizer {
-    if (!_swipeableView) {
-        return;
+  if (!_swipeableView) {
+    return;
+  }
+  CGPoint translation = [recognizer translationInView:_containerView];
+  CGPoint location = [recognizer locationInView:_containerView];
+  CGPoint velocity = [recognizer velocityInView:_containerView];
+  ZLSwipeableViewMovement *movement =
+  [[ZLSwipeableViewMovement alloc] initWithLocation:location
+                                         tranlation:translation
+                                           velocity:velocity];
+  
+  if (recognizer.state == UIGestureRecognizerStateBegan) {
+    [self setStateMoving:location];
+    [self didStartSwipingView:_view movement:movement];
+  }
+  
+  if (recognizer.state == UIGestureRecognizerStateChanged) {
+    [self setStateMoving:location];
+    [self swipingView:_view movement:movement];
+  }
+  
+  if (recognizer.state == UIGestureRecognizerStateEnded ||
+      recognizer.state == UIGestureRecognizerStateCancelled) {
+    if (_state != ViewManagerStateMoving) {
+      return;
     }
-    CGPoint translation = [recognizer translationInView:_containerView];
-    CGPoint location = [recognizer locationInView:_containerView];
-    CGPoint velocity = [recognizer velocityInView:_containerView];
-    ZLSwipeableViewMovement *movement =
-        [[ZLSwipeableViewMovement alloc] initWithLocation:location
-                                               tranlation:translation
-                                                 velocity:velocity];
-
-    if (recognizer.state == UIGestureRecognizerStateBegan) {
-        [self setStateMoving:location];
-        [self didStartSwipingView:_view movement:movement];
+    if ([_swipeableView.swipingDeterminator shouldSwipeView:_view
+                                                   movement:movement
+                                              swipeableView:_swipeableView]) {
+      CGVector directionVector = CGVectorFromCGPoint(CGPointMultiply(
+                                                                     CGPointNormalized(translation),
+                                                                     MAX(CGPointMagnitude(velocity), _swipeableView.minVelocityInPointPerSecond)));
+      [_swipeableView swipeTopViewFromPoint:location inDirection:directionVector];
+    } else {
+      [self setStateSnappingAtContainerViewCenter];
+      [self didCancelSwipingView:_view movement:movement];
     }
-
-    if (recognizer.state == UIGestureRecognizerStateChanged) {
-        [self setStateMoving:location];
-        [self swipingView:_view movement:movement];
-    }
-
-    if (recognizer.state == UIGestureRecognizerStateEnded ||
-        recognizer.state == UIGestureRecognizerStateCancelled) {
-        if (_state != ViewManagerStateMoving) {
-            return;
-        }
-        if ([_swipeableView.swipingDeterminator shouldSwipeView:_view
-                                                       movement:movement
-                                                  swipeableView:_swipeableView]) {
-            CGVector directionVector = CGVectorFromCGPoint(CGPointMultiply(
-                CGPointNormalized(translation),
-                MAX(CGPointMagnitude(velocity), _swipeableView.minVelocityInPointPerSecond)));
-            [_swipeableView swipeTopViewFromPoint:location inDirection:directionVector];
-        } else {
-            [self setStateSnappingAtContainerViewCenter];
-            [self didCancelSwipingView:_view movement:movement];
-        }
-
-        [self didEndSwipingView:_view movement:movement];
-    }
+    
+    [self didEndSwipingView:_view movement:movement];
+  }
 }
 
 - (void)setStateSnapping:(CGPoint)point {
-    if (_state == ViewManagerStateMoving) {
-        [self detachView];
-        [self snapView:point];
-    }
-    if (_state == ViewManagerStateSwiping) {
-        [self unpushView];
-        [self detachView];
-        [self snapView:point];
-    }
-    _state = ViewManagerStateSnapping;
+  if (_state == ViewManagerStateMoving) {
+    [self detachView];
+    [self snapView:point];
+  }
+  if (_state == ViewManagerStateSwiping) {
+    [self unpushView];
+    [self detachView];
+    [self snapView:point];
+  }
+  _state = ViewManagerStateSnapping;
 }
 
 - (void)setStateMoving:(CGPoint)point {
-    if (_state == ViewManagerStateSnapping) {
-        [self unsnapView];
-        [self attachView:point];
-    }
-    if (_state == ViewManagerStateMoving) {
-        [self moveView:point];
-    }
-    _state = ViewManagerStateMoving;
+  if (_state == ViewManagerStateSnapping) {
+    [self unsnapView];
+    [self attachView:point];
+  }
+  if (_state == ViewManagerStateMoving) {
+    [self moveView:point];
+  }
+  _state = ViewManagerStateMoving;
 }
 
 - (void)setStateSwiping:(CGPoint)point direction:(CGVector)directionVector {
-    if (_state == ViewManagerStateSnapping) {
-        [self unsnapView];
-        [self attachView:point];
-        [self pushViewFromPoint:point inDirection:directionVector];
-    }
-    if (_state == ViewManagerStateMoving) {
-        [self pushViewFromPoint:point inDirection:directionVector];
-    }
-    _state = ViewManagerStateSwiping;
+  if (_state == ViewManagerStateSnapping) {
+    [self unsnapView];
+    [self attachView:point];
+    [self pushViewFromPoint:point inDirection:directionVector];
+  }
+  if (_state == ViewManagerStateMoving) {
+    [self pushViewFromPoint:point inDirection:directionVector];
+  }
+  _state = ViewManagerStateSwiping;
 }
 
 - (void)setStateSnappingDefault {
-    [self setStateSnapping:[_view convertPoint:_view.center fromView:_view.superview]];
+  [self setStateSnapping:[_view convertPoint:_view.center fromView:_view.superview]];
 }
 
 - (void)setStateSnappingAtContainerViewCenter {
-    if (!_swipeableView) {
-        [self setStateSnappingDefault];
-        return;
-    }
-    [self setStateSnapping:[_containerView convertPoint:_swipeableView.center
-                                               fromView:_swipeableView.superview]];
+  if (!_swipeableView) {
+    [self setStateSnappingDefault];
+    return;
+  }
+  [self setStateSnapping:[_containerView convertPoint:_swipeableView.center
+                                             fromView:_swipeableView.superview]];
 }
 
 #pragma mark - UIDynamicAnimationHelpers
 
 - (void)snapView:(CGPoint)point {
-    _snapBehavior = [[UISnapBehavior alloc] initWithItem:_view snapToPoint:point];
-    _snapBehavior.damping = 0.75f; /* Medium oscillation */
-    [self addBehavior:_snapBehavior];
+  _snapBehavior = [[UISnapBehavior alloc] initWithItem:_view snapToPoint:point];
+  _snapBehavior.damping = 0.75f; /* Medium oscillation */
+  [self addBehavior:_snapBehavior];
 }
 
 - (void)unsnapView {
-    [self removeBehavior:_snapBehavior];
+  [self removeBehavior:_snapBehavior];
 }
 
 - (void)attachView:(CGPoint)point {
-    _anchorView.center = point;
-    _anchorView.backgroundColor = [UIColor blueColor];
-    _anchorView.hidden = YES;
-
-    // attach aView to anchorView
-    CGPoint p = _view.center;
-    _viewToAnchorViewAttachmentBehavior =
-        [[UIAttachmentBehavior alloc] initWithItem:_view
-                                  offsetFromCenter:UIOffsetMake(-(p.x - point.x), -(p.y - point.y))
-                                    attachedToItem:_anchorView
-                                  offsetFromCenter:UIOffsetZero];
-    _viewToAnchorViewAttachmentBehavior.length = 0;
-
-    // attach anchorView to point
-    _anchorViewToPointAttachmentBehavior = [[UIAttachmentBehavior alloc] initWithItem:_anchorView
-                                                                     offsetFromCenter:UIOffsetZero
-                                                                     attachedToAnchor:point];
-    _anchorViewToPointAttachmentBehavior.damping = 100;
-    _anchorViewToPointAttachmentBehavior.length = 0;
-
-    [self addBehavior:_viewToAnchorViewAttachmentBehavior];
-    [self addBehavior:_anchorViewToPointAttachmentBehavior];
+  _anchorView.center = point;
+  _anchorView.backgroundColor = [UIColor blueColor];
+  _anchorView.hidden = YES;
+  
+  // attach aView to anchorView
+  CGPoint p = _view.center;
+  _viewToAnchorViewAttachmentBehavior =
+  [[UIAttachmentBehavior alloc] initWithItem:_view
+                            offsetFromCenter:UIOffsetMake(-(p.x - point.x), -(p.y - point.y))
+                              attachedToItem:_anchorView
+                            offsetFromCenter:UIOffsetZero];
+  _viewToAnchorViewAttachmentBehavior.length = 0;
+  
+  // attach anchorView to point
+  _anchorViewToPointAttachmentBehavior = [[UIAttachmentBehavior alloc] initWithItem:_anchorView
+                                                                   offsetFromCenter:UIOffsetZero
+                                                                   attachedToAnchor:point];
+  _anchorViewToPointAttachmentBehavior.damping = 100;
+  _anchorViewToPointAttachmentBehavior.length = 0;
+  
+  _anchorViewDynamicItemBehavior = [[UIDynamicItemBehavior alloc] initWithItems:@[_view]];
+  _anchorViewDynamicItemBehavior.angularResistance = 1000;
+  _anchorViewDynamicItemBehavior.allowsRotation = NO;
+  
+  [self addBehavior:_viewToAnchorViewAttachmentBehavior];
+  [self addBehavior:_anchorViewToPointAttachmentBehavior];
+  [self addBehavior:_anchorViewDynamicItemBehavior];
 }
 
 - (void)moveView:(CGPoint)point {
-    if (!_viewToAnchorViewAttachmentBehavior || !_anchorViewToPointAttachmentBehavior) {
-        return;
-    }
-    _anchorViewToPointAttachmentBehavior.anchorPoint = point;
+  if (!_viewToAnchorViewAttachmentBehavior || !_anchorViewToPointAttachmentBehavior) {
+    return;
+  }
+  _anchorViewToPointAttachmentBehavior.anchorPoint = point;
 }
 
 - (void)detachView {
-    if (!_viewToAnchorViewAttachmentBehavior || !_anchorViewToPointAttachmentBehavior) {
-        return;
-    }
-    [self removeBehavior:_viewToAnchorViewAttachmentBehavior];
-    [self removeBehavior:_anchorViewToPointAttachmentBehavior];
+  if (!_viewToAnchorViewAttachmentBehavior || !_anchorViewToPointAttachmentBehavior) {
+    return;
+  }
+  [self removeBehavior:_viewToAnchorViewAttachmentBehavior];
+  [self removeBehavior:_anchorViewToPointAttachmentBehavior];
+  [self removeBehavior:_anchorViewDynamicItemBehavior];
 }
 
 - (void)pushViewFromPoint:(CGPoint)point inDirection:(CGVector)direction {
-    if (!_viewToAnchorViewAttachmentBehavior || !_anchorViewToPointAttachmentBehavior) {
-        return;
-    }
-
-    [self removeBehavior:_anchorViewToPointAttachmentBehavior];
-
-    _pushBehavior = [[UIPushBehavior alloc] initWithItems:@[ _anchorView ]
-                                                     mode:UIPushBehaviorModeInstantaneous];
-    _pushBehavior.pushDirection = direction;
-    [self addBehavior:_pushBehavior];
+  if (!_viewToAnchorViewAttachmentBehavior || !_anchorViewToPointAttachmentBehavior) {
+    return;
+  }
+  
+  [self removeBehavior:_anchorViewToPointAttachmentBehavior];
+  
+  CGSize viewSize = _view.frame.size;
+  _pushBehavior = [[UIPushBehavior alloc] initWithItems:@[ _anchorView ]
+                                                   mode:UIPushBehaviorModeInstantaneous];
+  _pushBehavior.pushDirection = direction;
+  _pushBehavior.magnitude = 0.01f * viewSize.width * viewSize.height;
+  [self addBehavior:_pushBehavior];
 }
 
 - (void)unpushView {
-    if (!_pushBehavior) {
-        return;
-    }
-    [self removeBehavior:_pushBehavior];
+  if (!_pushBehavior) {
+    return;
+  }
+  [self removeBehavior:_pushBehavior];
 }
 
 - (void)addBehavior:(UIDynamicBehavior *)behavior {
-    [_animator addBehavior:behavior];
+  [_animator addBehavior:behavior];
 }
 
 - (void)removeBehavior:(UIDynamicBehavior *)behavior {
-    [_animator removeBehavior:behavior];
+  [_animator removeBehavior:behavior];
 }
 
 #pragma mark - ()
 
 - (void)didStartSwipingView:(UIView *)view movement:(ZLSwipeableViewMovement *)movement {
-    if ([self.swipeableView.delegate
-            respondsToSelector:@selector(swipeableView:didStartSwipingView:atLocation:)]) {
-        [self.swipeableView.delegate swipeableView:self.swipeableView
-                               didStartSwipingView:view
-                                        atLocation:movement.location];
-    }
+  if ([self.swipeableView.delegate
+       respondsToSelector:@selector(swipeableView:didStartSwipingView:atLocation:)]) {
+    [self.swipeableView.delegate swipeableView:self.swipeableView
+                           didStartSwipingView:view
+                                    atLocation:movement.location];
+  }
 }
 
 - (void)swipingView:(UIView *)view movement:(ZLSwipeableViewMovement *)movement {
-    if ([self.swipeableView.delegate
-            respondsToSelector:@selector(swipeableView:swipingView:atLocation:translation:)]) {
-        [self.swipeableView.delegate swipeableView:self.swipeableView
-                                       swipingView:view
-                                        atLocation:movement.location
-                                       translation:movement.translation];
-    }
+  if ([self.swipeableView.delegate
+       respondsToSelector:@selector(swipeableView:swipingView:atLocation:translation:)]) {
+    [self.swipeableView.delegate swipeableView:self.swipeableView
+                                   swipingView:view
+                                    atLocation:movement.location
+                                   translation:movement.translation];
+  }
 }
 
 - (void)didCancelSwipingView:(UIView *)view movement:(ZLSwipeableViewMovement *)movement {
-    if ([self.swipeableView.delegate respondsToSelector:@selector(swipeableView:didCancelSwipe:)]) {
-        [self.swipeableView.delegate swipeableView:self.swipeableView didCancelSwipe:view];
-    }
+  if ([self.swipeableView.delegate respondsToSelector:@selector(swipeableView:didCancelSwipe:)]) {
+    [self.swipeableView.delegate swipeableView:self.swipeableView didCancelSwipe:view];
+  }
 }
 
 - (void)didEndSwipingView:(UIView *)view movement:(ZLSwipeableViewMovement *)movement {
-    if ([self.swipeableView.delegate
-            respondsToSelector:@selector(swipeableView:didEndSwipingView:atLocation:)]) {
-        [self.swipeableView.delegate swipeableView:self.swipeableView
-                                 didEndSwipingView:view
-                                        atLocation:movement.location];
-    }
+  if ([self.swipeableView.delegate
+       respondsToSelector:@selector(swipeableView:didEndSwipingView:atLocation:)]) {
+    [self.swipeableView.delegate swipeableView:self.swipeableView
+                             didEndSwipingView:view
+                                    atLocation:movement.location];
+  }
 }
 
 @end
+
